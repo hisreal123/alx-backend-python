026162ab06f1a6ba75f2e6412fd632c387b837be
hisreal123 <omosgoerge@gmail.com>
2024-10-10 23:25:26 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> tuple[float]:
+    """ typed-function that returns a float argument"""
+    return (a + b,)  # the , at the end makes is a tuple.
