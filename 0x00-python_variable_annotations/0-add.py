13dfa52e6e86df59c08a150b0cefd251ec0053d6
hisreal123 <omosgoerge@gmail.com>
2024-10-10 23:49:14 +0100

    task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+""" Description: type-annotated function add that takes a float a and a float b
+                 as arguments and returns their sum as a float
+    Arguments: a: float
+                b: float
+"""
 
-def add(a: float, b: float) -> tuple[float]:
-    """ typed-function that returns a float argument"""
-    return (a + b,)  # the , at the end makes is a tuple.
+
+def add(a: float, b: float) -> float:
+    """Adds two float numbers and returns the result as a tuple."""
+    return a + b  # the , at the end makes is a tuple.
